5e8210696a1d6650bf269d39643c4bfff3a86620
StefanErrorerko <nashes1320@gmail.com>
2024-03-14 13:44:31 +0200

    interval fixed

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -31,9 +31,9 @@ def process_agent_data(
     # Assuming classification logic based on accelerometer z-axis value ranges
     z_acceleration = agent_data.accelerometer.z
 
-    if interval["normal"]["start"] > z_acceleration < interval["normal"]["end"]:
+    if interval["normal"]["start"] < z_acceleration < interval["normal"]["end"]:
         road_state = "normal"
-    elif interval["less"]["start"] > z_acceleration < interval["less"]["end"] or interval["greater"]["start"] > z_acceleration < interval["greater"]["end"]:
+    elif interval["less"]["start"] < z_acceleration < interval["less"]["end"] or interval["greater"]["start"] > z_acceleration < interval["greater"]["end"]:
         road_state = "small pits"
     else:
         road_state = "large pits"
